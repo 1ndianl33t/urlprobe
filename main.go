d658123bff6d98d04fd669e7261562c353b2a5e5
Shiv Chouhan <shivchouhan679@gmail.com>
2020-06-20 11:52:03 +0530

    Added -s flag to print specific status code. & change colour output like 404 Red 403,401 Cyan

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ import (
         "time"
 )
 
+var concurrency int 
+var times int
+var status int 
+
 func Banner() {
         color.HiCyan(`
  ____ _____________.____   __________              ___.
@@ -18,13 +22,13 @@ func Banner() {
 |    |   /|       _/    |   |     ___/\_  __ \/  _ \| __ \_/ __ \
 |    |  / |    |   \    |___|    |     |  | \(  <_> ) \_\ \  ___/
 |______/  |____|_  /_______ \____|     |__|   \____/|___  /\___  >
-                 \/        \/                           \/     \/   V 2.0`)
-        color.HiYellow("           URLProbe:- Urls Status Code Checker")
+                 \/        \/                           \/     \/   V 2.1`)
+        color.HiYellow("           URLProbe:- Urls Status Code & ContentLength Checker")
         color.HiRed("              https://github.com/1ndianl33t")
 
         color.HiCyan("-------------------------------------------------------------------------")
 }
-func printStatus(req *http.Request, resp *http.Response, err error) {
+func printStatus(req *http.Request, resp *http.Response,  err error) {
         if err != nil {
                 return
         }
@@ -32,33 +36,53 @@ func printStatus(req *http.Request, resp *http.Response, err error) {
 
 }
 
+func ParseArguments() {
+    flag.IntVar(&concurrency, "c", 50, "Number of workers to use..default 50")
+    flag.IntVar(&status, "s", 1, "If enabled..then check for specific status")
+    flag.IntVar(&times, "t", 05, "Set rate limit")
+    flag.Parse()
+}
+
+
 func StatusCheck(req *http.Request, resp *http.Response) {
-        if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
-                color.HiGreen("[%d] L %d : %s\n", resp.StatusCode, resp.ContentLength, req.URL)
+        if status != 1 {
+            if status == resp.StatusCode {
+                if status == 404 {
+                    color.HiRed("[%d] L %d : %s\n", resp.StatusCode, resp.ContentLength, req.URL)
+                } 
+                if status != 404 {
+                    color.HiCyan("[%d] L %d : %s\n", resp.StatusCode, resp.ContentLength, req.URL)
+                } 
+                
+            }
+        } else {
+            if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+                    color.HiGreen("[%d] L %d : %s\n", resp.StatusCode, resp.ContentLength, req.URL)
 
-        }
-        if resp.StatusCode >= 300 && resp.StatusCode <= 308 {
-                color.HiBlue("[%d] L %d : %s\n", resp.StatusCode, resp.ContentLength, req.URL)
+            }
+            if resp.StatusCode >= 300 && resp.StatusCode <= 308 {
+                    color.HiBlue("[%d] L %d : %s\n", resp.StatusCode, resp.ContentLength, req.URL)
 
-        }
-        if resp.StatusCode >= 400 && resp.StatusCode <= 451 {
-                color.HiRed("[%d] L %d : %s\n", resp.StatusCode, resp.ContentLength, req.URL)
+            }
+            if resp.StatusCode >= 400 && resp.StatusCode <= 451 {
+                    if resp.StatusCode == 404 {
+                        color.HiRed("[%d] L %d : %s\n", resp.StatusCode, resp.ContentLength, req.URL)
+                    } else {
+                        color.HiCyan("[%d] L %d : %s\n", resp.StatusCode, resp.ContentLength, req.URL)
+                    }
+                    
+            }
 
-        }
+            if resp.StatusCode >= 500 && resp.StatusCode <= 511 {
+                    color.HiCyan("[%d] L %d : %s\n", resp.StatusCode, resp.ContentLength, req.URL)
 
-        if resp.StatusCode >= 500 && resp.StatusCode <= 511 {
-                color.HiCyan("[%d] L %d : %s\n", resp.StatusCode, resp.ContentLength, req.URL)
-
+            }
         }
 }
 
 func main() {
         Banner()
-        var concurrency = 50
-        flag.IntVar(&concurrency, "c", 50, "")
-        var times int = 05
-        flag.IntVar(&times, "t", 05, "")
-        flag.Parse()
+        ParseArguments()
         p := gahttp.NewPipeline()
         p.SetConcurrency(concurrency)
         p.SetRateLimit(time.Duration(times) * time.Second)
